c23140c5e75b5ef339bee3765b5d03868b82eda9
surajsahani9321 <radhashyam9321@gmail.com>
2024-07-03 17:01:32 +0530

    student-api-using-nodejs

--- db.js
+++ db.js
@@ -1,20 +1,23 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 require('dotenv').config();
-const REMOTE_DB_URL=process.env.REMOTE_DB_URL;
+const REMOTE_DB_URL = process.env.REMOTE_DB_URL;
 
-mongoose.connect(REMOTE_DB_URL);
-const db=mongoose.connection;
+mongoose.connect(REMOTE_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
 
-db.on('connected',function(){
+db.on('connected', function () {
     console.log("Database connected...");
 });
 
-db.on('disconnected',function(){
+db.on('disconnected', function () {
     console.log("Database disconnected...");
 });
 
-db.on('error',function(){
+db.on('error', function () {
     console.log("Something went wrong...");
 });
 
-module.exports=db;
+module.exports = db;
