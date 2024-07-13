fde2d0ce6a82dda4d19ddba09c8a62808051b8cb
surajsahani9321 <radhashyam9321@gmail.com>
2024-07-13 16:24:54 +0530

    student-api-using-nodejs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const studentRouter = require('./studentRouter');
 const branchRouter = require('./branchRouter');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 const PORT=process.env.PORT;
 
